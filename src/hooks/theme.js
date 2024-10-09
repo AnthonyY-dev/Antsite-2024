cc56624ca52795ed8b9f3f885c621e09415c79b6
Anthony Y <toha.yurynets@gmail.com>
2024-10-09 00:03:57 -0400

    Dark by Default

---
+++ src/hooks/theme.js
@@ -0,0 +1,11 @@
+// theme.js
+import { extendTheme } from "@chakra-ui/react";
+
+const customTheme = extendTheme({
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  },
+});
+
+export default customTheme;
