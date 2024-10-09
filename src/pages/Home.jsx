98a1f371ee3b5e02791941422a6dc5a578a545ea
Anthony Y <toha.yurynets@gmail.com>
2024-10-09 00:28:48 -0400

    Added empty notice

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
 import DrawerExample from "../components/SidebarDrawer";
-import { useDisclosure } from "@chakra-ui/react";
+import { useDisclosure, useColorModeValue } from "@chakra-ui/react";
 
 const Home = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const btnRef = React.useRef();
   return (
     <div style={{ height: "100vh" }}>
+      <h1
+        style={{
+          color: useColorModeValue("#242424", "white"),
+          fontSize: 50,
+          fontWeight: 600,
+        }}
+      >
+        Nothing here yet, come back later :)
+      </h1>
       <p>Home</p>
       <DrawerExample
         button={
